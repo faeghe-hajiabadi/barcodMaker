ef6d0f2ea7a83ed1f5e735de7d10fe2a09d26686
Faeghe <faeghe.hajiabadi@gmail.com>
2020-10-28 11:34:26 +0800

    add base component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import BarCodeGenerator from "./components/BarCodeGenarator/BarCodeGenerator";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BarCodeGenerator />;
 }
 
 export default App;
---
+++ src/components/BarCodeGenarator/BarCodeGenerator.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+
+
+function BarCodeGenerator() {
+  const initialState = {
+    NetworkName: "",
+    Password: "",
+    Encryption: {
+      None: "",
+      WPAWPA2: "",
+      WEP: ""
+    },
+    isMember: false
+  };
+  const [ value, setValue ] = useState(initialState);
+  const [formValue,setFormValue ] = useState('test')
+
+  const mySubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("this is form",event.target)
+    setFormValue(value)
+    console.log("value",value)
+    // setValue(event.target.value);
+  }
+  const myChangeHandler = (event) => {
+    setValue({...value,[event.target.name]: event.target.value})
+    console.log("value",{value})
+
+  }
+  const downloadQR = () => {
+    const canvas = document.getElementById("123456");
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = "123456.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+  const downloadQRSVG = ()=> {
+    convertCanvasToImage()
+  }
+  function convertCanvasToImage() {
+    const canvas = document.getElementById("123456");
+    var dataURL = svgDataURL(canvas);
+    var dl = document.createElement("a");
+    document.body.appendChild(dl); // This line makes it work in Firefox.
+    dl.setAttribute("href", dataURL);
+    dl.setAttribute("download", "test.svg");
+    dl.click();
+    
+    
+  }
+  function svgDataURL(svg) {
+      var svgAsXML = (new XMLSerializer).serializeToString(svg);
+      return "data:image/svg+xml," + encodeURIComponent(svgAsXML);
+    }
+  return (
+    <div className="App">
+      <form onSubmit={mySubmitHandler}>
+      
+      <input
+        type='text'
+        name='NetworkName'
+        onChange={myChangeHandler}
+      />
+       <input
+        type='password'
+        name='password'
+        onChange={myChangeHandler}
+      />
+      <input
+        type='submit'
+      />
+      </form>
+      <QRCode id='123456' value= {JSON.stringify(formValue)}/>
+ 
+     
+     <a onClick={downloadQR}> Download QR </a>
+     <a onClick={downloadQRSVG}> Download QR SVG </a>
+
+    </div>
+  );
+}
+
+export default BarCodeGenerator;
