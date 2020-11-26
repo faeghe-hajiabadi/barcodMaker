c447e46f2579f299ab5439fc5a9ba07b462d5d20
Faeghe <faeghe.hajiabadi@gmail.com>
2020-11-26 16:57:37 +0800

    choose color for qr code

--- src/components/BarCodeGenarator/BarCodeGenerator.js
+++ src/components/BarCodeGenarator/BarCodeGenerator.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 import "./barcodeGenerator.scss";
 import QRCode from "easyqrcodejs";
+import downArrow from "../../img/arrowDown.png";
 
 const App = styled.div`
   display: flex;
@@ -24,7 +25,7 @@ const ButtonsContainer = styled.div`
   width: 80%;
   align-items: center;
   justify-content: space-evenly;
-  margin-top: 20%;
+  margin-top: 10%;
 `;
 const DefaultBtn = css`
   height: 70px;
@@ -38,26 +39,28 @@ const SvgSave = styled.button`
   ${DefaultBtn};
   background: #fe9b06;
 `;
+const BarcodeTitleInput = styled.input`
+  width: 100%;
+  background: transparent;
+  height: 50px;
+  border: none;
+  color: white;
+  border-bottom: 2px solid #ddeaf9;
+  &::placeholder {
+    color: white;
+  }
+  &:focus-visible {
+    outline: none;
+  }
+`;
 const PngSave = styled.button`
   ${DefaultBtn};
   background: #3dbcf9;
 `;
-
+const BarcodeColorInput = styled.input`
+  ${BarcodeTitleInput};
+`;
 function BarCodeGenerator(props) {
-  const code = React.createRef();
-
-  const [options, setOptions] = useState({
-    text: "test meee btbtbtb",
-    width: 209,
-    height: 209,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-  });
-  useEffect(() => {
-    new QRCode(code.current, options);
-  }, [code, options]);
-
-  // const value = props;
   const initialState = {
     NetworkName: "",
     Password: "",
@@ -68,10 +71,58 @@ function BarCodeGenerator(props) {
     },
     isMember: false,
   };
+
   const [value, setValue] = useState(initialState);
   const [formValue, setFormValue] = useState("test");
   const [isOpen, setOpen] = useState([false, false, false]);
+  const [color, setColor] = useState({
+    darkColor: "#000000",
+    lightColor: "#FFFFFF",
+    topLeft: "#b7d28d",
+    topRight: "#c17e61",
+    bottomLeft: "#aa5b71",
+  });
 
+  const [qrOptions, setQrOptions] = useState({
+    text: "faeghe haji",
+    title: "",
+    width: 180,
+    height: 180,
+    colorDark: "#000000",
+    colorLight: "#FFFFFF",
+    PO_TL: "#b7d28d",
+    PO_TR: "#c17e61",
+    PO_BL: "#aa5b71",
+    logo:''
+  });
+
+  const [uploadedImage, setUploadedImage] = useState({
+    image: "",
+  });
+  const [qrcode, setQrcode] = useState();
+  const [qrcodeDOM, setQrcodeDOM] = useState();
+
+  useEffect(() => {
+    setQrcodeDOM(React.createRef());
+  }, {});
+
+  useEffect(() => {
+    console.log("qrcode", qrcode);
+    generate();
+  }, [qrOptions, qrcodeDOM]);
+
+  const generate = () => {
+    if (!qrcodeDOM) {
+      return;
+    }
+    console.log("qrcode", qrcode);
+    if (qrcode) {
+      console.log("not empty", qrcode);
+      qrcode.clear();
+    }
+
+    setQrcode(new QRCode(qrcodeDOM.current, qrOptions));
+  };
   const mySubmitHandler = (event) => {
     event.preventDefault();
     console.log("this is form", event.target);
@@ -79,10 +130,10 @@ function BarCodeGenerator(props) {
     console.log("value", value);
     // setValue(event.target.value);
   };
-  const myChangeHandler = (event) => {
-    setValue({ ...value, [event.target.name]: event.target.value });
-    console.log("value", { value });
-  };
+  // const myChangeHandler = (event) => {
+  //   setValue({ ...value, [event.target.name]: event.target.value });
+  //   console.log("value", { value });
+  // };
   const downloadQR = () => {
     const canvas = document.getElementById("123456");
     const pngUrl = canvas
@@ -112,54 +163,150 @@ function BarCodeGenerator(props) {
     return "data:image/svg+xml," + encodeURIComponent(svgAsXML);
   }
   const imageSettingsCode = { height: 20, width: 60, excavate: true };
+
   const toggleAccordion = (index) => {
-    console.log("this isINDEX; ", index);
-    setOpen((isOpen[index] = !isOpen[index]));
+    console.log("this isINDEX in parent; ", index);
+    const newArr = [...isOpen];
+    newArr[index] = !newArr[index];
+    for (let i = 0; i < newArr.length; i++) {
+      if (i !== index) {
+        newArr[i] = false;
+      }
+    }
+    console.log("this is new state", newArr);
+    setOpen(newArr);
   };
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      setUploadedImage(file);
+      setQrOptions((prevState) => ({
+        ...prevState,
+        logo: uploadedImage,
+      }));
+    }
+  };
+
+  console.log("this is state", qrOptions);
   return (
     <App>
       <BarcodeContainer>
-        <div ref={code}></div>
+        <span id="qrcode" ref={qrcodeDOM}></span>
 
-        <form onSubmit={mySubmitHandler}>
+        <form className="barcodegenerator-form" onSubmit={mySubmitHandler}>
           <div className="wrapper">
             <Accordion
-              title="Frame"
-              toggleAccordion={toggleAccordion}
+              title="Title"
+              toggleAccordion={() => toggleAccordion(0)}
               isOpen={isOpen[0]}
-              index={0}
             >
-              Sunlight reaches Earth's atmosphere and is scattered in all
-              directions by all the gases and particles in the air. Blue light
-              is scattered more than the other colors because it travels as
-              shorter, smaller waves. This is why we see a blue sky most of the
-              time.
+              <BarcodeTitleInput placeholder="Enter your barcode title here"></BarcodeTitleInput>
             </Accordion>
             <Accordion
               title="Shape"
-              toggleAccordion={toggleAccordion}
+              toggleAccordion={() => toggleAccordion(1)}
               isOpen={isOpen[1]}
-              index={0}
             >
-              It's really hot inside Jupiter! No one knows exactly how hot, but
-              scientists think it could be about 43,000°F (24,000°C) near
-              Jupiter's center, or core.
+              <div className="accordion-content-child">
+                <span>Dark Color</span>
+                <BarcodeColorInput
+                  type="color"
+                  id="favcolor"
+                  name="favcolor"
+                  value={qrOptions.colorDark}
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    setQrOptions((prevState) => ({
+                      ...prevState,
+                      colorDark: value,
+                    }));
+                  }}
+                ></BarcodeColorInput>
+              </div>
+              <div className="accordion-content-child">
+                <span>Light Color</span>
+                <BarcodeColorInput
+                  type="color"
+                  id="favcolor"
+                  name="favcolor"
+                  value={qrOptions.colorLight}
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    setQrOptions((prevState) => ({
+                      ...prevState,
+                      colorLight: value,
+                    }));
+                  }}
+                ></BarcodeColorInput>
+              </div>
+              <div className="accordion-content-child">
+                <span>Top-right Color</span>
+                <BarcodeColorInput
+                  type="color"
+                  id="favcolor"
+                  name="favcolor"
+                  value={qrOptions.PO_TL}
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    setQrOptions((prevState) => ({
+                      ...prevState,
+                      PO_TL: value,
+                    }));
+                  }}
+                ></BarcodeColorInput>
+              </div>
+              <div className="accordion-content-child">
+                <span>Top-left Color</span>
+                <BarcodeColorInput
+                  type="color"
+                  id="favcolor"
+                  name="favcolor"
+                  value={qrOptions.PO_TR}
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    setQrOptions((prevState) => ({
+                      ...prevState,
+                      PO_TR: value,
+                    }));
+                  }}
+                ></BarcodeColorInput>
+              </div>
+              <div className="accordion-content-child">
+                <span>Bottom-left Color</span>
+                <BarcodeColorInput
+                  type="color"
+                  id="favcolor"
+                  name="favcolor"
+                  value={qrOptions.PO_BL}
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    setQrOptions((prevState) => ({
+                      ...prevState,
+                      PO_BL: value,
+                    }));
+                  }}
+                ></BarcodeColorInput>
+              </div>
             </Accordion>
+
             <Accordion
               title="Logo"
-              toggleAccordion={toggleAccordion}
+              toggleAccordion={() => toggleAccordion(2)}
               isOpen={isOpen[2]}
-              index={0}
             >
-              A black hole is an area of such immense gravity that nothing --
-              not even light -- can escape from it.
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                multiple="false"
+              />
             </Accordion>
           </div>
+          <ButtonsContainer>
+            <SvgSave>Save SVG</SvgSave>
+            <PngSave>Save Png</PngSave>
+          </ButtonsContainer>
         </form>
-        <ButtonsContainer>
-          <SvgSave>Save SVG</SvgSave>
-          <PngSave>Save Png</PngSave>
-        </ButtonsContainer>
 
         {/* <a onClick={downloadQR}> Download QR </a>
         <a onClick={downloadQRSVG}> Download QR SVG </a> */}
@@ -170,16 +317,18 @@ function BarCodeGenerator(props) {
 const Accordion = ({ title, index, children, isOpen, toggleAccordion }) => {
   // const [isOpen, setOpen] = React.useState(isOpen);
   function toggleAccordionChild() {
+    console.log("clicked in child component");
     toggleAccordion();
   }
   return (
     <div className="accordion-wrapper">
-      <div
-        className={`accordion-title ${isOpen ? "open" : ""}`}
-        onClick={(index) => toggleAccordionChild(index)}
-      >
-        {title}
+      <div className="title-wrapper" onClick={toggleAccordionChild}>
+        <div className={`accordion-title ${isOpen ? "open" : ""}`}>
+          <div>{title}</div>
+        </div>
+        <img width="10" src={downArrow}></img>
       </div>
+
       <div className={`accordion-item ${!isOpen ? "collapsed" : ""}`}>
         <div className="accordion-content">{children}</div>
       </div>
